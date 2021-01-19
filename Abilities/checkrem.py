23d863a99ecfefe28e6d6e4e6f68d06c3efa6705
Krishiv <krishivgamer777@gmail.com>
2021-01-19 18:17:52 +0530

    Initial commit:wq

--- Abilities/checkrem.py
+++ Abilities/checkrem.py
@@ -1,23 +1,26 @@
 from . import set_reminder
-from . import rem
 import datetime
 from plyer import notification
 from . import speakandlisten
+import json
 
 notification_icon = r'C:\Users\HP\Desktop\krishiv\Assistant\Abilities\notification.ico'
 
+with open('rem' + '.json', 'r') as r:
+    cr = json.load(r)
+
 while True:
         #checking if the time of the datime input is equal to current time
-        if datetime.datetime.now().strftime("%I:%M %p") == rem['time']:
+        if datetime.datetime.now().strftime("%I:%M %p") == cr['time']:
             #checking if the date of the datetime input in equal to currrent datetime
-            if datetime.datetime.now().strftime("%d-%m-%Y") == rem['date']:
+            if datetime.datetime.now().strftime("%d-%m-%Y") == cr['date']:
                 speakandlisten.speak(set_reminder.m)
                 notification.notify(
                     
                     #defining title as t
-                    title = rem['title'],    
+                    title = cr['title'],    
                     #defining message as m
-                    message = rem['message'],
+                    message = cr['message'],
                     #defining the notification icon
                     app_icon = notification_icon,
                     #seting the timeout
