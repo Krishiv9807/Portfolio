558e572be0bd8bf59fbed99b8798e54c0a1cb4da
Krishiv <krishivgamer777@gmail.com>
2021-01-22 12:17:02 +0530

    clean files

--- Abilities/gettime.py
+++
@@ -1,5 +0,0 @@
-import datetime
-
-def get_time():
-    #grtting the time
-    return  datetime.datetime.now().strftime("%I:%M %p")
--- Abilities/way_wycd_info.py
+++
@@ -1,8 +0,0 @@
-from . import speakandlisten
-
-def way_info():
-    speakandlisten.speak("My name is Jarvis. I am your AI Assistant")
-
-def wcyd_info():
-    print("I can set reminders, play songs, make google, wikipedia and youtube searches, recharge your phone and many more things.")
-    speakandlisten.speak("I can set reminders, play songs, make google, wikipedia and youtube searches, recharge your phone and many more things.")
